c558d8bf7462a6469f8b2870bf77eade13531b05
kiko <fcpenet@gmail.com>
2020-08-15 18:58:21 +0800

    initial commit

---
+++ covidTracker/src/app/services/firebase.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  private snapshotChangesSubscription: any;
+
+  constructor(
+    public afs: AngularFirestore,
+    public afAuth: AngularFireAuth
+  ) { }
+  addPatient(value){
+    return new Promise<any>((resolve, reject) => {
+      let currentUser = firebase.auth().currentUser;
+      this.afs.collection('patients').doc(currentUser.uid).collection('tasks').add({
+        title: value.title,
+        description: value.description,
+        image: value.image
+      })
+      .then(
+        res => resolve(res),
+        err => reject(err)
+      )
+    })
+  }
+
+  getPatients(){
+    return new Promise<any>((resolve, reject) => {
+      this.afAuth.user.subscribe(currentUser => {
+        if(currentUser){
+          this.snapshotChangesSubscription = this.afs.collection('people').doc(currentUser.uid).collection('tasks').snapshotChanges();
+          resolve(this.snapshotChangesSubscription);
+        }
+      })
+    });
+  }
+
+  unsubscribeOnLogOut(){
+    //remember to unsubscribe from the snapshotChanges
+    this.snapshotChangesSubscription.unsubscribe();
+  }
+
+}
